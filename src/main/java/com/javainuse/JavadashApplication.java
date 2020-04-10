f4993d8f3c6865ce50214420dd2041b3411857e9
navedbc <navedbc@gmail.com>
2020-04-10 15:05:08 -0700

    initial commit

---
+++ src/main/java/com/javainuse/JavadashApplication.java
@@ -0,0 +1,15 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+public class JavadashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadashApplication.class, args);
+	}
+
+}
